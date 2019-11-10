261b32929abbe9afc5047a9ee78cef60d063c25b
Patrick Friedman <patrick.s.friedman@gmail.com>
2019-11-10 05:22:19 -0600

    Add files via upload

--- htmlParcer.py
+++ htmlParcer.py
@@ -1,46 +1,10 @@
 import re, requests, string
 from bs4 import BeautifulSoup
 
-def obj() :
-    with open('objective.txt') as file1:
-        for line in file1:
-            url = line
-            res = requests.get(url)
-            html_page = res.content
-            soup = BeautifulSoup(html_page, 'html.parser')
-            text = soup.find_all(text=True)
-
-            output = ''
-            blacklist = [
-                '[document]',
-                'noscript',
-                'header',
-                'html',
-                'meta',
-                'head', 
-                'input',
-                'script',
-                'style',
-            ]
-
-            for t in text:
-                if t.parent.name not in blacklist:
-                    output += '{} '.format(t)
-            regex = re.compile('[^a-zA-Z +]')
-            output = regex.sub('',output)
-
-            sep = 'The Times is committed to publishing  a diversity of letters  to the editor. We’d like to hear what you think about this or any of our articles.'
-            output = output.split(sep, 1)[0]
-
-            return str(output)
-            # for x in sortFreq(words(output)) :
-            #     print(str(x))
-            # print()
-        
-    file1.close()
-
 def sub() :
     with open('subjective.txt') as file1:
+        articleCollection = []
+        superString = ''
         for line in file1:
             url = line
             res = requests.get(url)
@@ -64,10 +28,97 @@ def sub() :
             for t in text:
                 if t.parent.name not in blacklist:
                     output += '{} '.format(t)
-            regex = re.compile('[^a-zA-Z +]')
+            regex = re.compile('[^a-zA-Z ]')
             output = regex.sub('',output)
+            output = output.lower()
 
-            print(output)
+            articleCollection.append(output.split(' '))
+            superString = superString + output
+
+        freqList = []
+        for x in sortFreq(words(superString)) :
+            freqList.append(x)
+
+        newList = []
+        for each in freqList:
+            newList.append(each[1])
+
+        convertedCollection = []
+        for article in articleCollection:
+            tokenizedArticle = []
+            for word in article:
+                if newList.index(word) != 0:
+                    tokenizedArticle.append(newList.index(word))
+            convertedCollection.append(tokenizedArticle)
+        
+        finalSub = open('finalSub.txt','w')
+        for each in convertedCollection:
+            for nums in each:
+                finalSub.write(str(nums))
+                finalSub.write(' ')
+            finalSub.write('\n')
+        
+        finalSub.close()
+    file1.close()
+
+def obj() :
+    with open('objective.txt') as file1:
+        articleCollection = []
+        superString = ''
+        for line in file1:
+            url = line
+            res = requests.get(url)
+            html_page = res.content
+            soup = BeautifulSoup(html_page, 'html.parser')
+            text = soup.find_all(text=True)
+
+            output = ''
+            blacklist = [
+                '[document]',
+                'noscript',
+                'header',
+                'html',
+                'meta',
+                'head', 
+                'input',
+                'script',
+                'style',
+            ]
+
+            for t in text:
+                if t.parent.name not in blacklist:
+                    output += '{} '.format(t)
+            regex = re.compile('[^a-zA-Z ]')
+            output = regex.sub('',output)
+            output = output.lower()
+
+            articleCollection.append(output.split(' '))
+            superString = superString + output
+
+        freqList = []
+        for x in sortFreq(words(superString)) :
+            freqList.append(x)
+
+        newList = []
+        for each in freqList:
+            newList.append(each[1])
+
+        convertedCollection = []
+        for article in articleCollection:
+            tokenizedArticle = []
+            for word in article:
+                if newList.index(word) != 0:
+                    tokenizedArticle.append(newList.index(word))
+            convertedCollection.append(tokenizedArticle)
+
+        finalObj = open('finalObj.txt','w')
+        for each in convertedCollection:
+            for nums in each:
+                finalObj.write(str(nums))
+                finalObj.write(' ')
+            finalObj.write('\n')
+
+        finalObj.close()
     file1.close()
 
 def words(output) :
@@ -88,6 +139,5 @@ def sortFreq(freq) :
     sort.reverse()
     return sort
 
+sub()
 obj()
-print()
-#sub()
