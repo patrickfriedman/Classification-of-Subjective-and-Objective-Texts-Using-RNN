ac1a07a469c8be232d72518a5dfb5485b9832377
Jan Morgenstern <janmorgensternnls@gmail.com>
2019-11-10 12:55:00 -0600

    Presentation scraper

--- webScraper.py
+++ webScraper.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import os
 import time
-from ics import Calendar,Event
 import datetime
 
 
@@ -12,12 +11,12 @@ def showMoreNY(passInDriver):
     for i in range(len(showMore)):
         if "show more" in showMore[i].text.lower():
             try:
-                print(showMore[i].click())
+                showMore[i].click()
                 time.sleep(2)
 
             except:
                 # time.sleep(1)
-                print(showMore[i].click())
+                showMore[i].click()
 
 def getNYArticles(passInDriver):
     articles=[]
@@ -35,11 +34,11 @@ def showMoreNPR(passInDriver):
     for i in range(len(showMore)):
         if "load more" in showMore[i].text.lower():
             try:
-                print(showMore[i].click())
+                showMore[i].click()
                 # time.sleep(1)
             except:
                 time.sleep(1)
-                print(showMore[i].click())
+                showMore[i].click()
 
 
 def getNPRArticles(passInDriver):
@@ -70,42 +69,51 @@ def main():
     # put driver executable file in the script directory
     chrome_driver = os.path.join(os.getcwd(), "chromedriver")
 
-    # objective1 = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    objective1 = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
     # objective2 = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-    subjective1 = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    # subjective1 = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
 
-    # objective1.get("https://www.nytimes.com/section/politics")
-    # objective2.get("https://www.npr.org/sections/politics/")
-    # subjective1.get("https://www.nytimes.com/section/opinion/politics")
-    # sum=0
-    # for i in range(100):
-    #     # if sum%3==0:
-        # showMoreNY(subjective1)
-    #     # if sum%3==1:
-    # showMoreNY(objective1)
-        # if sum%3==2:
-        # showMoreNPR(objective2)
-        #
-        # time.sleep(1)
-    # getNYArticles(subjective1)
-    # getNYArticles(objective1)
+    for i in range(3):
+        if i ==0:
+            print("Getting objective articles...\n")
+            objective1.get("https://www.nytimes.com/section/politics")
+        if i==1:
+            objective1.get("https://www.npr.org/sections/politics/")
+        if i==2:
+            print("\n\nGetting subjective articles...\n")
+            objective1.get("https://www.nytimes.com/section/opinion/politics")
+        for x in range(5):
+            if i ==0:
+                showMoreNY(objective1)
 
-    # getNPRArticles(objective2)
-    #
-    # getNYArticles(subjective1)
+            if i==1:
+                showMoreNPR(objective1)
 
-    # for i in range(300):
-    #     try:
-    #         showMoreNY(objective1)
-    #         showMoreNPR(objective2)
-    #     except:
-    #         continue
+            if i==2:
+                showMoreNY(objective1)
+                time.sleep(1)
+                showMoreNY(objective1)
+                time.sleep(1)
+                showMoreNY(objective1)
+                time.sleep(1)
+                showMoreNY(objective1)
+        if i ==0:
+            getNYArticles(objective1)
+        if i==1:
+            getNPRArticles(objective1)
+        if i==2:
+            getNYArticles(objective1)
+
+
+
+
+
 
 
 
     # Go through each event entry, gather data and determine whether they are valid entries
     #   If they are, print their information and add them to the cal (Calendar()) object
-    sum=0
+
 
 
 
