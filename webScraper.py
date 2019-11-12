7838c016515deafc195a492cca23bdfec27bc7bc
Jan Morgenstern <janmorgensternnls@gmail.com>
2019-11-12 09:33:30 -0600

    Updated article retrieval

--- webScraper.py
+++ webScraper.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import os
+import os.path
+from os import path
 import time
 import datetime
 
@@ -20,10 +22,9 @@ def showMoreNY(passInDriver):
 
 def getNYArticles(passInDriver):
     articles=[]
-    objective1Articles=passInDriver.find_elements_by_tag_name("a")
-    for i in range(len(objective1Articles)):
-        # eventObject = Event()
-        eventLink=objective1Articles[i].get_attribute('href')
+    NYArticles=passInDriver.find_elements_by_tag_name("a")
+    for i in range(len(NYArticles)):
+        eventLink=NYArticles[i].get_attribute('href')
         if ".html" in eventLink and eventLink not in articles and eventLink[25].isdigit():
             print(eventLink)
             articles.append(eventLink)
@@ -35,31 +36,61 @@ def showMoreNPR(passInDriver):
         if "load more" in showMore[i].text.lower():
             try:
                 showMore[i].click()
-                # time.sleep(1)
+                time.sleep(2)
             except:
-                time.sleep(1)
+                # time.sleep(1)
                 showMore[i].click()
 
 
 def getNPRArticles(passInDriver):
 
-    # passInDriver=passInDriver.find_elements_by_class_name("teaser")
-    objective1Articles=passInDriver.find_elements_by_tag_name("a")
-    # objective1Articles=passInDriver.
+    NPRArticles=passInDriver.find_elements_by_tag_name("a")
     articles=[]
-    for i in range(len(objective1Articles)):
+    for i in range(len(NPRArticles)):
 
-        # eventObject = Event()
         try:
-            eventLink=objective1Articles[i].get_attribute('href')
-            if type(eventLink)==str and "www.npr.org" in eventLink and len(eventLink)>25 and (eventLink not in articles) and eventLink[25].isdigit():
+            eventLink=NPRArticles[i].get_attribute('href')
+            if type(eventLink)==str and "www.npr.org" in eventLink and len(eventLink)>30 and (eventLink not in articles):
                 articles.append(eventLink)
                 print(eventLink)
         except:
             continue
     return articles
+def getNPRArchives(passInDriver):
+
+
+def readFoundArticles(txtFileString):
+    articles=[]
+    if path.exists(txtFileString):
+        txtFile=open(txtFileString,"r")
+        lines=txtFile.readlines()
+        for line in lines:
+            articles.append(line)
+    return articles
+
+def writeFoundArticles(txtFileString,articles):
+    txtFile=open(txtFileString,"a+")
+    for article in articles:
+        txtFile.write(article+"\n")
+def removeDuplicates(txtFileString,articles):
+    alreadyFound=readFoundArticles(txtFileString)
+    if len(alreadyFound)<len(articles):
+        for article in alreadyFound:
+            if articles.count(article)>0:
+                articles.remove(article)
+    elif len(alreadyFound)>len(articles):
+        for article in articles:
+            if alreadyFound.count(article)>0:
+                articles.remove(article)
+    else:
+        for article in articles:
+            if alreadyFound.count(article)>0:
+                articles.remove(article)
+
+    return articles
 
 def main():
+
 
     # cal=Calendar()
     chrome_options = Options()
@@ -69,40 +100,45 @@ def main():
     # put driver executable file in the script directory
     chrome_driver = os.path.join(os.getcwd(), "chromedriver")
 
-    objective1 = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
     # objective2 = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
     # subjective1 = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
 
     for i in range(3):
         if i ==0:
             print("Getting objective articles...\n")
-            objective1.get("https://www.nytimes.com/section/politics")
+            driver.get("https://www.nytimes.com/section/politics")
         if i==1:
-            objective1.get("https://www.npr.org/sections/politics/")
+            driver.get("https://www.npr.org/sections/politics/")
         if i==2:
             print("\n\nGetting subjective articles...\n")
-            objective1.get("https://www.nytimes.com/section/opinion/politics")
-        for x in range(5):
+            driver.get("https://www.nytimes.com/section/opinion/politics")
+        clickedMore=0
+        for x in range(100):
             if i ==0:
-                showMoreNY(objective1)
-
+                showMoreNY(driver)
+                clickedMore=clickedMore+1
+                print("Clicked show more "+ str(clickedMore)+" time.")
             if i==1:
-                showMoreNPR(objective1)
-
+                showMoreNPR(driver)
+                clickedMore=clickedMore+1
+                print("Clicked load more "+ str(clickedMore)+" time.")
             if i==2:
-                showMoreNY(objective1)
-                time.sleep(1)
-                showMoreNY(objective1)
-                time.sleep(1)
-                showMoreNY(objective1)
-                time.sleep(1)
-                showMoreNY(objective1)
+                showMoreNY(driver)
+                clickedMore=clickedMore+1
+                print("Clicked show more "+ str(clickedMore)+" time.")
         if i ==0:
-            getNYArticles(objective1)
+            articles=getNYArticles(driver)
+            articles=removeDuplicates("objective.txt",articles)
+            writeFoundArticles("objective.txt",articles)
         if i==1:
-            getNPRArticles(objective1)
+            articles=getNPRArticles(driver)
+            articles=removeDuplicates("objective.txt",articles)
+            writeFoundArticles("objective.txt",articles)
         if i==2:
-            getNYArticles(objective1)
+            articles=getNYArticles(driver)
+            articles=removeDuplicates("subjective.txt",articles)
+            writeFoundArticles("subjective.txt",articles)
 
 
 
@@ -111,8 +147,6 @@ def main():
 
 
 
-    # Go through each event entry, gather data and determine whether they are valid entries
-    #   If they are, print their information and add them to the cal (Calendar()) object
 
 
 
